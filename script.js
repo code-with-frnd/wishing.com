c4e15ce23e23f5f9712e5162c27b2891e016da19
code-with-frnd <workgd6@gmail.com>
2024-08-01 12:10:08 +0530

    addded file of wishing

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var audio = document.getElementById('myAudio');
+        audio.play();
+    }, 3000); // Delay of 3 seconds (3000 milliseconds)
+});
